3850974e0dc2a8f19f8d0b5ff6c3f212df072a68
Ernestas Vaiciukevicius <e.vaiciukevicius@adform.com>
2021-06-04 17:12:15 +0300

    3 point bed levelling

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1852,12 +1852,12 @@
  * Override if the automatically selected points are inadequate.
  */
 #if EITHER(AUTO_BED_LEVELING_3POINT, AUTO_BED_LEVELING_UBL)
-  //#define PROBE_PT_1_X 15
-  //#define PROBE_PT_1_Y 180
-  //#define PROBE_PT_2_X 15
-  //#define PROBE_PT_2_Y 20
-  //#define PROBE_PT_3_X 170
-  //#define PROBE_PT_3_Y 20
+  #define PROBE_PT_1_X 5
+  #define PROBE_PT_1_Y 210  
+  #define PROBE_PT_2_X 153
+  #define PROBE_PT_2_Y 210
+  #define PROBE_PT_3_X (60-27)
+  #define PROBE_PT_3_Y 0
 #endif
 
 /**
