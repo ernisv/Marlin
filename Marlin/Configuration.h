7db1d05683ca3c0729b582d306ed55fe0bd577b2
Ernestas Vaiciukevicius <ernisv@gmail.com>
2023-01-12 08:31:48 +0200

    soft bounds corrected

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1695,16 +1695,16 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 160
-#define Y_BED_SIZE 160
+#define X_BED_SIZE 170
+#define Y_BED_SIZE 170
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
-#define X_MIN_POS 0
-#define Y_MIN_POS 0
+#define X_MIN_POS -X_BED_SIZE/2
+#define Y_MIN_POS -Y_BED_SIZE/2
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 200
+#define X_MAX_POS X_BED_SIZE/2
+#define Y_MAX_POS Y_BED_SIZE/2
+#define Z_MAX_POS 400
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
 //#define J_MIN_POS 0
